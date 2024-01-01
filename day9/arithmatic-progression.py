99a195b29cd46d0332b6e7e8de822561134234f5
Gaurav Vijayvargia <gaurav@opentech.eco>
2024-01-01 00:15:07 -0800

    AoC Day 9 python

---
+++ day9/arithmatic-progression.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import unittest
+
+
+def get_level_0_nth(n, start):
+    # Level 0 is where the differences are all 0
+    return 0
+
+def get_level_1_nth(n, start):
+    # Level 1 is where the differences are all constant
+    return start
+
+def get_level_2_nth(n, start, diff):
+    # Level 2 is where the differences are all in arithmatic progression
+    # with a constant difference
+    if n < 0:
+        return 0
+    level_1_first = get_level_1_nth(0, diff)
+    return start + n  * level_1_first
+
+def get_level_3_nth(n, start, diff, diff_of_2):
+    # Level 3 is where the differenc of differences are all in 
+    # arithmatic progression with a constant difference
+    if n < 0:
+        return 0
+
+    level_2_first = get_level_2_nth(0, diff, diff_of_2)
+    level_2_n_minus_1 = get_level_2_nth(n-1, diff, diff_of_2)
+    return start +   (n  / 2) * (level_2_first + level_2_n_minus_1)
+
+def get_level_4_nth(n, start, diff, diff_of_2, diff_of_3):
+    # Level 3 is where the differenc of differences are all in 
+    # arithmatic progression with a constant difference
+    level_3_first = get_level_3_nth(0, diff, diff_of_2, diff_of_3)
+    level_2_first = get_level_2_nth(0, diff_of_2, diff_of_3)
+    level_2_n_minus_2 = get_level_2_nth(n-2, diff_of_2, diff_of_3)
+
+    if n < 1:
+        level_2_first = 0
+
+    nth_value = start + (n ) * level_3_first + (n*(n-1)) /2 * level_2_first  + (n-2) / 2 * (level_2_first + level_2_n_minus_2)
+
+    return nth_value
+
+class TestSolution(unittest.TestCase):
+
+    def test_one(self):
+        # Testing series 3, 9, 15, 21, 27, 33, 39, 45, 51, 57
+        self.assertEqual(get_level_2_nth(0, 3, 6), 3)
+        self.assertEqual(get_level_2_nth(1, 3, 6), 10)
+
+
+    def test_two(self):
+        # testing series 1 3 6 10 15 21
+        self.assertEqual(get_level_3_nth(0, 1, 2, 1), 1)
+        self.assertEqual(get_level_3_nth(1, 1, 2, 1), 3)
+        self.assertEqual(get_level_3_nth(2, 1, 2, 1), 6)
+        self.assertEqual(get_level_3_nth(3, 1, 2, 1), 10)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+
+        print(get_level_4_nth(i, 10, 3, 0, 2))
+
---
+++ day9/solution.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import unittest
+
+
+class Solution:
+    def __init__(self, filename, part_to_run=1):
+        """
+        :param filename: The name of the file to read input from
+        :param part_to_run: The part of the solution to run (1 or 2)
+        """
+        self.filename = filename
+        self.part_to_run = part_to_run
+
+    def extract_input(self):
+        with open(self.filename) as infile:
+            lines = infile.readlines()
+        return lines
+
+    def run(self):
+        def get_diffs(input):
+            diffs = []
+            for i in range(1, len(input)):
+                diffs.append(input[i] - input[i - 1])
+            return diffs
+
+        def is_predictable(diffs):
+            return all(x == diffs[0] for x in diffs)
+
+
+        input = self.extract_input()
+
+        history_sum = 0
+        for history in input:
+            hist_list = list(map(int, history.split()))
+
+            if self.part_to_run == 2:
+                hist_list = list(reversed(hist_list))
+            difflist = [hist_list]
+            while not is_predictable(difflist[-1]):
+                difflist.append(get_diffs(difflist[-1]))
+
+            nextitem = 0
+            while len(difflist) > 1:
+                currdiff = difflist.pop()
+                nextitem = difflist[-1][-1] + currdiff[-1]
+                difflist[-1].append(nextitem)
+            history_sum += nextitem
+
+
+        return history_sum
+
+
+
+if __name__ == "__main__":
+    solution = Solution("input.txt",2)
+    print(solution.run())
